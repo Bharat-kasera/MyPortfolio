112016c0587d90fca85bb2ab991453606ea38c0a
Bharat Kasera <bharat.codewolf@gmail.com>
2023-07-05 00:45:50 +0530

    first commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,7 +4,7 @@ import './experience.css';
 
 const Experience = () => {
   return (
-    <section id="experience">
+    <section id="experience" >
       <h5>The Skills I Have</h5>
       <h2>Skills</h2>
       <div className="container experience__container">
@@ -29,12 +29,12 @@ const Experience = () => {
             </article>
             <article className="experience__details">
               <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Next.js</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4>Redux</h4>
             </article>
-            {/* <article className="experience__details">
-              <BsFillPatchCheckFill className="experience__details-icon" />
-              <h4>SCSS</h4>
-            </article> */}
             <article className="experience__details">
               <BsFillPatchCheckFill className="experience__details-icon" />
               <h4>Tailwind CSS</h4>
@@ -93,3 +93,134 @@ const Experience = () => {
 }
 
 export default Experience
+// import { useState } from "react";
+// import { BsFillPatchCheckFill } from "react-icons/bs";
+// import "./experience.css";
+
+// const Experience = () => {
+//   const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 });
+
+//   const handleMouseMove = (e) => {
+//     const cardElements = document.getElementsByClassName("card");
+//     const newMouseCoordinates = {
+//       x: e.clientX,
+//       y: e.clientY,
+//     };
+//     setMouseCoordinates(newMouseCoordinates);
+
+//     for (const card1 of cardElements) {
+//       const rect = card1.getBoundingClientRect();
+//       const x = newMouseCoordinates.x - rect.left;
+//       const y = newMouseCoordinates.y - rect.top;
+
+//       card1.style.setProperty("--mouse-x", `${x}px`);
+//       card1.style.setProperty("--mouse-y", `${y}px`);
+//     }
+//   };
+//   return (
+//     <section id="experience">
+//       <h5>The Skills I Have</h5>
+//       <h2>Skills</h2>
+//       <div id="cards" onMouseMove={handleMouseMove}>
+//         <div class="card1">
+//           <div class="card-content">
+//             <div class="card-image">
+//               <i class="fa-duotone fa-apartment"></i>
+//             </div>
+//             <div class="card-info-wrapper">
+//               <div class="card-info">
+//                 <i class="fa-duotone fa-apartment"></i>
+//                 <div class="card-info-title">
+//                   <h3>Apartments</h3>
+//                   <h4>Places to be apart. Wait, what?</h4>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="card1">
+//           <div class="card-content">
+//             <div class="card-image">
+//               <i class="fa-duotone fa-unicorn"></i>
+//             </div>
+//             <div class="card-info-wrapper">
+//               <div class="card-info">
+//                 <i class="fa-duotone fa-unicorn"></i>
+//                 <div class="card-info-title">
+//                   <h3>Unicorns</h3>
+//                   <h4>A single corn. Er, I mean horn.</h4>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="card1">
+//           <div class="card-content">
+//             <div class="card-image">
+//               <i class="fa-duotone fa-blender-phone"></i>
+//             </div>
+//             <div class="card-info-wrapper">
+//               <div class="card-info">
+//                 <i class="fa-duotone fa-blender-phone"></i>
+//                 <div class="card-info-title">
+//                   <h3>Blender Phones</h3>
+//                   <h4>These absolutely deserve to exist.</h4>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="card1">
+//           <div class="card-content">
+//             <div class="card-image">
+//               <i class="fa-duotone fa-person-to-portal"></i>
+//             </div>
+//             <div class="card-info-wrapper">
+//               <div class="card-info">
+//                 <i class="fa-duotone fa-person-to-portal"></i>
+//                 <div class="card-info-title">
+//                   <h3>Adios</h3>
+//                   <h4>See you...</h4>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="card1">
+//           <div class="card-content">
+//             <div class="card-image">
+//               <i class="fa-duotone fa-person-from-portal"></i>
+//             </div>
+//             <div class="card-info-wrapper">
+//               <div class="card-info">
+//                 <i class="fa-duotone fa-person-from-portal"></i>
+//                 <div class="card-info-title">
+//                   <h3>I mean hello</h3>
+//                   <h4>...over here.</h4>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="card1">
+//           <div class="card-content">
+//             <div class="card-image">
+//               <i class="fa-duotone fa-otter"></i>
+//             </div>
+//             <div class="card-info-wrapper">
+//               <div class="card-info">
+//                 <i class="fa-duotone fa-otter"></i>
+//                 <div class="card-info-title">
+//                   <h3>Otters</h3>
+//                   <h4>Look at me, imma cute lil fella.</h4>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Experience;
